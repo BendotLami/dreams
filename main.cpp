31e64e0ce82fbd2f15a4ebaee88d26d6031970e4
BendotLami <bendotlami@gmail.com>
2023-09-01 11:23:47 +0300

    Added logic to Game

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <algorithm>
+#include <any>
 #include <functional>
 #include <iostream>
 #include <iterator>
 #include <list>
+#include <optional>
 #include <random>
+#include <ranges>
 #include <sstream>
 #include <variant>
 #include <vector>
-// class Card {};
 
 template <typename... Ts> struct Overload : Ts... {
   using Ts::operator()...;
@@ -46,6 +48,7 @@ private:
 };
 
 class QueenFactory {
+public:
   static std::vector<Queen> getQueens() {
     std::vector<Queen> q;
     q.emplace_back(15, Queen::Type::Cat);
@@ -120,7 +123,7 @@ public:
     return s;
   }
 
-  int getQueenCount() { return queens.size(); }
+  int getQueenCount() const { return queens.size(); }
 
   std::string printCards() const {
 
@@ -147,7 +150,9 @@ public:
     return s;
   }
 
-  Queen stealQueen(int idx) {
+  void addQueen(Queen q) { this->queens.emplace_back(std::move(q)); }
+
+  Queen removeQueen(int idx) {
     auto it = queens.begin();
     std::advance(it, idx);
     Queen tmp = *it;
@@ -296,12 +301,17 @@ class Game {
   }
 
 public:
-  Game(int players) { initCards(players); };
+  Game(int players) {
+    initCards(players);
+    std::vector<Queen> tmp = QueenFactory::getQueens();
+    std::transform(tmp.begin(), tmp.end(), std::back_inserter(queens),
+                   [](Queen &q) { return std::pair<Queen, bool>(q, true); });
+  };
 
   std::string playersString() {
     std::stringstream ss;
     for (int i = 0; i < players.size(); i++) {
-      ss << "Player " << i + 1 << ":\n";
+      ss << "Player " << i << ":\n";
       ss << players[i] << "\n";
     }
     return ss.str();
@@ -309,16 +319,38 @@ public:
 
   void printPlayers() { std::cout << playersString(); }
 
-  void playKnight(int currentPlayer) {
+  std::string queensString() {
+    std::string msg;
+    for_each(queens.begin(), queens.end(), [&msg](std::pair<Queen, bool> a) {
+      msg.append(a.second ? a.first.toString() : "_");
+      msg.push_back(' ');
+    });
+    msg.pop_back();
+
+    return msg;
+  }
+
+  bool playKnight(int currentPlayer) {
     std::stringstream msg;
 
-    msg << "Players"
-        << "\n";
-    msg << playersString();
-    msg << "Insert which player you want to attack: ";
-    int attackPlayerIdx = readInput(msg.str(), [this, currentPlayer](int i) {
-      return i >= 0 && i < players.size() && i != currentPlayer;
-    });
+    if (!anyPlayerHasQueens()) {
+      std::cout << "No player have any queens." << std::endl;
+      return false;
+    }
+
+    bool hasQueens = false;
+    int attackPlayerIdx;
+    while (!hasQueens) {
+      msg << "Players"
+          << "\n";
+      msg << playersString();
+      msg << "Insert which player you want to attack: ";
+      attackPlayerIdx = readInput(msg.str(), [this, currentPlayer](int i) {
+        return i >= 0 && i < players.size() && i != currentPlayer;
+      });
+      hasQueens = players[attackPlayerIdx].getQueenCount() > 0;
+      std::cout << "Player does not have queens." << std::endl;
+    }
 
     msg.str(std::string());
     msg << players[attackPlayerIdx].printQueens() << '\n';
@@ -326,6 +358,42 @@ public:
     int queenIdx = readInput(msg.str(), [this, attackPlayerIdx](int i) {
       return i >= 0 && i < players[attackPlayerIdx].getQueenCount();
     });
+
+    players[currentPlayer].addQueen(
+        players[attackPlayerIdx].removeQueen(queenIdx));
+
+    return true;
+  }
+
+  bool playKing(int currentPlayer) {
+    std::stringstream msg;
+
+    std::vector<int> range;
+    for (int i = 0; i < queens.size(); i++)
+      if (queens[i].second)
+        range.push_back(i);
+
+    if (range.size() == 0) {
+      std::cout << "No hidden queens available" << std::endl;
+      return false;
+    }
+
+    auto isQueenIdxValid = [&range](int i) {
+      for (const auto j : range)
+        if (i == j)
+          return true;
+      return false;
+    };
+
+    msg << "Hidden Queens:\n";
+    msg << queensString();
+    msg << "\n"
+        << "Insert chosen queen: ";
+
+    int input = readInput(msg.str(), isQueenIdxValid);
+    players[currentPlayer].addQueen(wakeQueen(input).value());
+
+    return true;
   }
 
   void PlayTurn(int playerIdx) {
@@ -335,23 +403,51 @@ public:
     msg << player;
     msg << "Insert which card you want to play: ";
 
-    int cardIdx = readInput(
-        msg.str(), [this](int i) { return i >= 0 && i < CardsPerPlayer; });
+    bool res = false;
+    int cardIdx;
 
-    Card playedCard = player.playCard(cardIdx, deck.pop_card());
-    std::cout << printCard(playedCard) << std::endl;
+    while (!res) {
+      int cardIdx = readInput(
+          msg.str(), [this](int i) { return i >= 0 && i < CardsPerPlayer; });
 
-    std::visit(Overload{[](Number n) { return; },
-                        [this, playerIdx](Knight k) { playKnight(playerIdx); },
-                        [](auto c) {
-                          std::cout << "errer: " << printCard(c) << std::endl;
-                        }},
-               playedCard);
+      // Card playedCard = player.playCard(cardIdx, deck.pop_card());
+      Card peekedCard = player.peekCard(cardIdx);
+      std::cout << printCard(peekedCard) << std::endl;
+
+      res = std::visit(
+          Overload{
+              [](Number n) { return true; },
+              [this, playerIdx](Knight k) { return playKnight(playerIdx); },
+              [this, playerIdx](King k) { return playKing(playerIdx); },
+              [](auto c) {
+                std::cout << "errer: " << printCard(c) << std::endl;
+                return true;
+              }},
+          peekedCard);
+    }
+
+    // if (res)
+    player.playCard(cardIdx, deck.pop_card());
   }
 
 private:
+  bool anyPlayerHasQueens() {
+    return std::any_of(players.begin(), players.end(),
+                       [](const Player &p) { return p.getQueenCount() > 0; });
+  }
+
+  std::optional<Queen> wakeQueen(int idx) {
+    if (queens[idx].second == false)
+      return {};
+    queens[idx].second = false;
+    return queens[idx].first;
+  }
+
+  void sleepyQueen(int idx) { queens[idx].second = true; }
+
   Deck deck;
   std::vector<Player> players;
+  std::vector<std::pair<Queen, bool>> queens;
 };
 
 int main() {
@@ -359,6 +455,7 @@ int main() {
   Game g(3);
   g.printPlayers();
   g.PlayTurn(0);
+  g.printPlayers();
 
   return 0;
 }
